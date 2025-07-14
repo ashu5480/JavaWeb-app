9d365d53ca5daaeda28117d9b2559a27b2144193
ashutosh -singh <singhashu772@gmail.com>
2025-07-14 23:14:49 +0530

    Second one

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello from Servlet!</h1>");
+        out.println("<h1>Hello from Servlet   !</h1>");
     }
 }
